53fe1615799e861b0ca4d1a71f6428e42194f187
Michael Rudd <audiostratus@yahoo.com>
2020-06-12 14:52:01 -0500

    Update popup.js

--- popup/popup.js
+++ popup/popup.js
@@ -1,1 +1,15 @@
+class Product{
+  name = "";
+  units = {temperature: "F", speed: "KT", distance: "M"};
+}
 
+class Graphic extends Product{
+  type = "";
+  northBound = 0;
+  southBound = 0;
+  westBound = 0;
+  eastBound = 0;
+}
+
+class Alphanumeric extends Product{
+}
